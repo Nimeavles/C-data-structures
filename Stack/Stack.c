93e05fc158181985eff7ab8e205dab94be898089
Nimeavles <prlf505@gmail.com>
2023-02-04 14:13:45 +0100

    fix: 🩹length name on stack struct

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -7,7 +7,7 @@ struct Stack {
   int *first;
   int *last;
   int *stack_items[MAX_BUFF];
-  int lenght;
+  int length;
 };
 
 void append(struct Stack *stack, int *value_to_add) {
@@ -15,28 +15,28 @@ void append(struct Stack *stack, int *value_to_add) {
     stack->last = value_to_add;
   }
   stack->first = value_to_add;
-  stack->stack_items[stack->lenght] = value_to_add;
-  stack->lenght++;
+  stack->stack_items[stack->length] = value_to_add;
+  stack->length++;
 }
 
 void pop(struct Stack *stack) {
-  if (stack->lenght <= 0) {
+  if (stack->length <= 0) {
     printf("No value to delete\n");
     exit(-1);
   }
 
-  stack->first = stack->stack_items[stack->lenght - 1];
-  stack->lenght--; 
-  stack->stack_items[stack->lenght] = NULL;
+  stack->first = stack->stack_items[stack->length - 1];
+  stack->length--; 
+  stack->stack_items[stack->length] = NULL;
 }
 
 void print_stack(struct Stack *stack) {
-  if (stack->lenght == 0) {
+  if (stack->length == 0) {
     printf("No elements to print\n");
     exit(0);
   }
 
-  for (int i = 0; i < stack->lenght; i++) {
+  for (int i = 0; i < stack->length; i++) {
     printf("%i\n", *stack->stack_items[i]);
   }
 }
